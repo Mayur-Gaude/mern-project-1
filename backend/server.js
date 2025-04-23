b1799805f1f31c61dfb7a8a7106d348767b7271d
Mayur-Gaude <kunited633@gmail.com>
2025-04-23 23:47:44 +0530

    fix: added the app.get(/*) path

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
     
     // Serve the static files from the React app
-    // app.get('/*', (req, res) => {
-    //   res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    // });
+    app.get('/*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
   } else {
     app.get('/', (req, res) => {
       res.send("API is running...");
